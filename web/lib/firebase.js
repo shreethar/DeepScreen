d9c87d9afcf885bda52ffbc9f5622b080e8e8da5
Shreethar <shree80537901@gmail.com>
2026-01-11 19:01:27 +0800

    Revoked API key & changed it

--- web/lib/firebase.js
+++ web/lib/firebase.js
@@ -11,13 +11,13 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyADYJtwhKiON4b7MQy9df77-QCXGXWsEYA",
-    authDomain: "workshop-resume.firebaseapp.com",
-    projectId: "workshop-resume",
-    storageBucket: "workshop-resume.firebasestorage.app",
-    messagingSenderId: "1013839348174",
-    appId: "1:1013839348174:web:d41c08e81019c915052b9f",
-    measurementId: "G-HXPE491SWG"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase only if it hasn't been initialized already
